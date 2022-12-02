1380163b1d4c55438db967cbb1bf99014961cf7b
Gonzalo Cortes <gonzalocorte99@gmail.com>
2022-12-02 14:31:16 -0300

    primera preentrega del final

---
+++ main.js
@@ -0,0 +1,17 @@
+let nombre = parseFloat(prompt("Ingrese su Nombre"));
+let Apellido = parseFloat(prompt("Ingrese su Apellido"));
+let  nombreAutor = parseFloat(prompt("Ingrese el autor que Busca"));
+let  generoLibro = parseFloat(prompt("Que genero estas Buscado??"));
+let precio = 500 / ( precio * precio );
+
+alert("tu precio es: "  + precio.toFixed(2) + "$");
+
+if (precio < $650,59) {
+alert("tu precio es bajo sigue comprando!");
+} else if ((precio <= $650,59) && (precio >= $750,44)) {
+ alert("precio Normal");
+} else if ((precio >= 790,99) && (precio <= 765,39)) {
+    alert("tu precio es mayor o cerca de tu limite");
+}else {
+    alert("limite alcanzado");
+}
