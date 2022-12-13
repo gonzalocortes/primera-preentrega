42277e6319a023dae82c658e3057347cfe403cd2
Gonzalo Cortes <gonzalocorte99@gmail.com>
2022-12-13 20:17:31 -0300

    segunda pre entrega proceso

--- main.js
+++ main.js
@@ -1,27 +1,60 @@
+const libros = [{
+    id: 1,
+    autor: "Bram Stoker",
+    titulo: "Dracula",
+    genero: "terror,suspenso",
+    precio: "2000",
+    stock: 17,
+},
+{
+    id: 2,
+    autor: "Robert louis",
+    titulo: "el extraño doctor",
+    precio: "1500",
+    stock: 19,
+},
+
+{
+    id: 3,
+    autor: "Susan hill",
+    titulo: "la dama de negro",
+    precio: "1600",
+    stock: 15,
+},
+
+{
+    id: 4,
+    autor: "Henry James",
+    titulo: "otra vuelta de tuerca",
+    precio: "2500",
+    stock: 16,
+}
+];
+
 let nombre = (prompt("Ingrese su Nombre"));
 let Apellido = (prompt("Ingrese su Apellido"));
 let nombreAutor = (prompt("Ingrese el autor que Busca"));
 let generoLibro = (prompt("Que genero estas Buscado??"));
 let montoLibro = parseFloat(prompt("costo del libro??"));
 const iva = 0.21;
-const productoslibros = [];
+
 class producto {
-  construcotor (id, Nombre, precio, stock) {
-    this.id = id;
-    this.Nombre = Nombre;
-    this.precio = precio;
-    this.stock = stock;
-    this.iva = 21;
-  }
-  aplicarIVA() {
-    this.precio = this.precio + ((this.precio * this.iva) / 100);
-  }
+    constructor(id, Nombre, precio, stock) {
+        this.id = id;
+        this.Nombre = Nombre;
+        this.precio = precio;
+        this.stock = stock;
+        this.iva = 21;
+    }
+    aplicarIVA() {
+        this.precio = this.precio + ((this.precio * this.iva) / 100);
+    }
 }
 let cargarProducto = true;
 
 while (cargarProducto != null) {
-    let id_producto = productoslibros.length + 1;
-    let producto = new producto(id_producto, nombre_producto, precio_producto, stock_producto);
+    let id_producto = libros.length + 1;
+    let producto = new producto(producto, nombre, precio, stock);
     console.log(producto);
 }
 
