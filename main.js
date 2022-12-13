05544819f5e1bcd070a6bb4976431824a460aa17
Gonzalo Cortes <gonzalocorte99@gmail.com>
2022-12-12 22:48:04 -0300

    segunda preentrega proceso

--- main.js
+++ main.js
@@ -4,6 +4,27 @@ let nombreAutor = (prompt("Ingrese el autor que Busca"));
 let generoLibro = (prompt("Que genero estas Buscado??"));
 let montoLibro = parseFloat(prompt("costo del libro??"));
 const iva = 0.21;
+const productoslibros = [];
+class producto {
+  construcotor (id, Nombre, precio, stock) {
+    this.id = id;
+    this.Nombre = Nombre;
+    this.precio = precio;
+    this.stock = stock;
+    this.iva = 21;
+  }
+  aplicarIVA() {
+    this.precio = this.precio + ((this.precio * this.iva) / 100);
+  }
+}
+let cargarProducto = true;
+
+while (cargarProducto != null) {
+    let id_producto = productoslibros.length + 1;
+    let producto = new producto(id_producto, nombre_producto, precio_producto, stock_producto);
+    console.log(producto);
+}
+
 let precioA = 900;
 let precioB = 750;
 let precioC = 630;
